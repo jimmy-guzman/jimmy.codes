24479cdbcdbe4aad0c37be06ef33fad90650862b
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2023-03-30 20:05:34 -0500

    fix: 🐛 remove fathom custom domain :( by using fathom-client (#135)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from 'next/app'
 
 import { Wrapper, Container, Footer } from '../components/atoms'
 import { Navigation } from '../components/molecules'
+import { useFathom } from '../hooks/use-fathom'
 
 import '../styles/global.css'
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useFathom()
+
   return (
     <Wrapper>
       <Navigation />
