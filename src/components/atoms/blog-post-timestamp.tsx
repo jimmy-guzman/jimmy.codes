6d42a66efc1441e910dedb443d3a44b8e8cac5b2
jimmy-guzman <hi@jimmyguzman.com>
2023-10-02 19:40:19 -0500

    feat: ✨ improve last updated timestamp styling on mobile

---
+++ src/components/atoms/blog-post-timestamp.tsx
@@ -0,0 +1,18 @@
+export const BlogPostTimestamp = ({
+  timestamp,
+}: {
+  timestamp: Date
+}): JSX.Element => {
+  return (
+    <div className='flex justify-center text-sm italic text-slate-400 md:justify-end md:text-base'>
+      <time dateTime={timestamp.toISOString()}>
+        Last updated on{' '}
+        {timestamp.toLocaleDateString('en', {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        })}
+      </time>
+    </div>
+  )
+}
