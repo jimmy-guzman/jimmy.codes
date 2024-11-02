52d76ad450aa3986e116cdf152a36669cbb91c0b
jimmy-guzman <hi@jimmyguzman.com>
2024-11-02 10:49:20 -0500

    feat: ✨ bump `next` to v15

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  experimental: {
-    serverComponentsExternalPackages: ["@napi-rs/simple-git"],
-  },
   images: {
     remotePatterns: [{ hostname: "res.cloudinary.com" }],
   },
   reactStrictMode: true,
+  serverExternalPackages: ["@napi-rs/simple-git"],
 };
 
 export default nextConfig;
