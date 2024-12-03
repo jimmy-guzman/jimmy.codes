f22c00a1ad83d3fbaff949e1adc9c4c5478bebfd
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-12-02 19:49:10 -0700

    ci: 👷 simplify eslint config by removing override (#361)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,31 +1,3 @@
 import eslintConfig from "@jimmy.codes/eslint-config";
-// @ts-expect-error missing types
-import nextPlugin from "@next/eslint-plugin-next";
 
-export default eslintConfig(
-  {
-    testing: {
-      framework: "jest",
-    },
-  },
-  {
-    files: ["**/layout.tsx", "**/page.tsx"],
-    name: "react-refresh/next",
-    rules: {
-      "react-refresh/only-export-components": "off",
-    },
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    name: "next",
-    plugins: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- missing types
-      "@next/next": nextPlugin,
-    },
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- missing types
-    rules: {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- missing types
-      ...nextPlugin.configs["core-web-vitals"].rules,
-    },
-  },
-);
+export default eslintConfig();
