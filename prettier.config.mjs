78d18fef58f467f51cd741bf6e04a0440f4111fb
jimmy-guzman <hi@jimmy.codes>
2025-09-03 16:28:23 -0500

    feat: ✨ drop notes

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,5 +2,5 @@ export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   astroSkipFrontmatter: true,
   tailwindStylesheet: "./src/styles/global.css",
-  astroAllowShorthand: true,
+  astroAllowShorthand: false,
 };
