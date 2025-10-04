defdd044e524b1814f7c4581167a374042b03b09
jimmy-guzman <hi@jimmy.codes>
2025-10-04 12:47:36 -0500

    feat: ✨ add Questions or Feedback? to post

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -2,4 +2,5 @@ export const urls = {
   site: "https://jimmy.codes",
   LinkedIn: "https://www.linkedin.com/in/jimmy-guzman-moreno",
   GitHub: "https://github.com/jimmy-guzman",
-} satisfies Record<string, `https://${string}`>;
+  email: "mailto:hi@jimmy.codes",
+} satisfies Record<string, `https://${string}` | `mailto:${string}@${string}`>;
