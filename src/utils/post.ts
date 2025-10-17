3016891ee2898e16e59ed248ad09103f744a16bf
jimmy-guzman <hi@jimmy.codes>
2025-10-17 16:42:01 -0500

    feat: ✨ give links a bit more action

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,9 +1,4 @@
-import type { InferEntrySchema } from "astro:content";
-
-interface Post {
-  slug: string;
-  data: InferEntrySchema<"posts">;
-}
+import type { CollectionEntry } from "astro:content";
 
 /**
  * Get the short title for a post, preferring the heading if available.
@@ -11,6 +6,12 @@ interface Post {
  * @param post The post to get the short title for
  * @returns The short title of the post
  */
-export const shortTitle = (post: Post) => {
+export const shortTitle = (post: Pick<CollectionEntry<"posts">, "data">) => {
   return post.data.shortTitle ?? post.data.title;
+};
+
+export const getPostByYear = (posts: CollectionEntry<"posts">[]) => {
+  return Object.entries(
+    Object.groupBy(posts, (post) => post.data.publishDate.getFullYear()),
+  ).sort(([a], [b]) => Number.parseInt(b, 10) - Number.parseInt(a, 10));
 };
