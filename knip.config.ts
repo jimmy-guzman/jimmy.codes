87c86053abc785350ba69d97044a43e940b86688
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2025-11-06 12:16:42 -0600

    feat: ✨ add Prevent TypeScript from Inferring blog post (#569)

--- knip.config.ts
+++ knip.config.ts
@@ -7,6 +7,7 @@ export default {
     "@commitlint/cli",
     "markdownlint",
     "astro:env", // TODO: remove when https://github.com/webpro-nl/knip/pull/1331 is merged
+    "@types/react", // used only for type declarations within twoslash code blocks
   ],
   ignoreBinaries: ["pandoc"],
 } satisfies KnipConfig;
