5b26920af2a682e18ebdb4406710de462602d1c4
jimmy-guzman <hi@jimmy.codes>
2025-11-06 12:45:49 -0600

    fix: 🐛 use correct config for fonts

--- knip.config.ts
+++ knip.config.ts
@@ -6,7 +6,6 @@ export default {
     "gitzy",
     "@commitlint/cli",
     "markdownlint",
-    "astro:env", // TODO: remove when https://github.com/webpro-nl/knip/pull/1331 is merged
     "@types/react", // used only for type declarations within twoslash code blocks
   ],
   ignoreBinaries: ["pandoc"],
