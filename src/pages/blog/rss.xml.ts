b350187140f2f5cb6b01e508eb615b5e11b1105e
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2026-03-16 07:37:40 -0500

    fix: 🐛 add missing image to json-ld (#719)

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -6,8 +6,15 @@ import { pages } from "@/configs/pages";
 
 export async function GET(context: { site: string }) {
   const blog = await getCollection("posts");
+  const publishDates = blog.map(({ data }) => data.publishDate.getTime());
+  const latestTimestamp = blog.length ? Math.max(...publishDates) : Date.now();
+  const lastBuildDate = new Date(latestTimestamp).toUTCString();
 
   return rss({
+    customData: [
+      `<atom:link href="${new URL("blog/rss.xml", context.site)}" rel="self" type="application/rss+xml" />`,
+      `<lastBuildDate>${lastBuildDate}</lastBuildDate>`,
+    ].join(""),
     description: pages.rss.description,
     items: blog.map((post) => ({
       categories: post.data.tags,
@@ -18,5 +25,7 @@ export async function GET(context: { site: string }) {
     })),
     site: context.site,
     title: pages.rss.title,
+    trailingSlash: false,
+    xmlns: { atom: "http://www.w3.org/2005/Atom" },
   });
 }
